24a676fb9420e77884897633bc8045cb599117b3
Avinash <avinashsinha751@gmail.com>
2024-09-12 23:53:34 +0600

    added vergon animation

--- src/components/Kosten.jsx
+++ src/components/Kosten.jsx
@@ -4,29 +4,28 @@ import { FaArrowRight, FaCheck } from "react-icons/fa";
 
 const Kosten = () => {
   return (
-    <div className="w-full h-full  flex flex-col lg:flex-row items-center justify-center overflow-hidden text-white">
+    <div className="w-full h-full  flex flex-col lg:flex-row items-center justify-center overflow-hidden  lg:gap-0 gap-8 text-white">
       {/* Left Section */}
-      <div className="w-full lg:w-2/3 flex items-start justify-center flex-col bg-[#AEBD48] p-6 lg:px-20 lg:py-12 h-full">
-  <h1 className="text-5xl lg:text-8xl mt-4 lg:mt-8 mb-4 lg:mb-8">
-    Was kostet es?
-  </h1>
-  <p className="text-lg lg:text-3xl mb-1 w-2/3">
-    Medisuite ist komplett kostenlos. Ihre Bewohner profitieren von
-    unseren attraktiven Heim­preisen. Vereinbaren Sie jetzt eine
-    un­verbindliche Vorführung.
-  </p>
-  {/* <p className="text-lg lg:text-3xl mb-1">anbinden und zentral  über eine</p>
-  <p className="text-lg lg:text-3xl mb-1">Plattform bestellen.</p> */}
-   <button className="flex items-center space-x-2 mt-4 bg-black border border-none text-white font-bold md:px-4 md:py-2 px-2 py-1 text-xs rounded-full">
-    <span>Demoanfragen</span>
-    <FaArrowRight />
-  </button>
-</div>  
+      <div className="w-full lg:w-2/3 flex items-start justify-center flex-col bg-[#AEBD48] py-6 px-8 md:px-16 h-full">
+        <h1 className="text-5xl lg:text-8xl mt-4 lg:mt-8 mb-4 lg:mb-8">
+          Was kostet es?
+        </h1>
+        <p className="text-lg lg:text-3xl mb-1 lg:w-4/5 w-full md:w-2/3">
+          Medisuite ist komplett kostenlos. Ihre Bewohner profitieren von
+          unseren attraktiven Heim­preisen. Vereinbaren Sie jetzt eine
+          un­verbindliche Vorführung.
+        </p>
+      
+        <button className="flex items-center space-x-2 mt-4 bg-black border border-none text-white font-bold md:px-6 md:py-4 px-2 py-1 text-base rounded-full">
+          <span>Demoanfragen</span>
+          <FaArrowRight />
+        </button>
+      </div>
 
       {/* Right Section */}
-      <div className="w-full lg:w-1/3 lg:bg-white bg-[#AEBD48] p-6 lg:relative flex justify-center items-center lg:justify-end">
+      <div className="w-full lg:w-1/3 lg:bg-white bg-[#AEBD48] px-8 md:px-16 md:py-4 lg:py-0 lg:px-0  lg:relative flex justify-center items-center lg:justify-end">
         <img
-          className="w-72 h-72 lg:w-96 lg:h-96 lg:absolute lg:-left-32  object-contain"
+          className="w-96 h-96 lg:w-96 lg:h-96 xl:w-[420px] xl:h-[420px] lg:absolute lg:-left-24 xl:-left-36  object-contain"
           src={screenbottom}
           alt="Medisuite"
         />
@@ -36,3 +35,32 @@ const Kosten = () => {
 };
 
 export default Kosten;
+
+{
+  /* <div className="w-full h-full  flex flex-col lg:flex-row items-center justify-center overflow-hidden text-white">
+
+<div className="w-full lg:w-2/3 flex items-start justify-center flex-col bg-[#AEBD48] p-6 lg:px-20 lg:py-12 h-full">
+<h1 className="text-5xl lg:text-8xl mt-4 lg:mt-8 mb-4 lg:mb-8">
+Was kostet es?
+</h1>
+<p className="text-lg lg:text-3xl mb-1 w-2/3">
+Medisuite ist komplett kostenlos. Ihre Bewohner profitieren von
+unseren attraktiven Heim­preisen. Vereinbaren Sie jetzt eine
+un­verbindliche Vorführung.
+</p>
+
+<button className="flex items-center space-x-2 mt-4 bg-black border border-none text-white font-bold md:px-4 md:py-2 px-2 py-1 text-xs rounded-full">
+<span>Demoanfragen</span>
+<FaArrowRight />
+</button>
+</div>  
+
+
+<div className="w-full lg:w-1/3 lg:bg-white bg-[#AEBD48] p-6 lg:relative flex justify-center items-center lg:justify-end">
+  <img
+    className="w-72 h-72 lg:w-96 lg:h-96 lg:absolute lg:-left-32  object-contain"
+    src={screenbottom}
+    alt="Medisuite"
+  />
+</div> */
+}
--- src/components/Verordnungen.jsx
+++ src/components/Verordnungen.jsx
@@ -1,169 +1,130 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { FaCheck } from "react-icons/fa";
 import arrow from "/arrow.png";
 
 const Verordnungen = () => {
-  const [values, setValues] = useState([
-    {
-      name: "Meier, Hans",
-      current: 0,
-      total: 408.01,
-      color: "bg-white",
-      colors: {
-        10: "bg-red-500",
-        30: "bg-[#a8bf00]",
-        60: "bg-[#fff027]",
-        80: "bg-[#ff7927]",
-        100: "bg-[#f05b56]",
-      },
-    },
-    {
-      name: "Müller, Erika",
-      current: 0,
-      total: 867.28,
-      color: "bg-white",
-      colors: {
-        30: "bg-[#22d3ee]",
-        60: "bg-[#3b82f6]",
-        80: "bg-[#8b5cf6]",
-        100: "bg-[#f472b6]",
-      },
-    },
-    {
-      name: "Schmidt, Elisabeth",
-      current: 0,
-      total: 408.01,
-      color: "bg-white",
-      colors: {
-        30: "bg-[#facc15]",
-        60: "bg-[#fb923c]",
-        80: "bg-[#ef4444]",
-        100: "bg-[#991b1b]",
-      },
-    },
-    {
-      name: "Fischer, Werner",
-      current: 0,
-      total: 199.5,
-      color: "bg-white",
-      colors: {
-        30: "bg-white",
-        60: "bg-white",
-        80: "bg-[#6ee7b7]",
-        100: "bg-[#d1fae5]",
-      },
-    },
-  ]);
+  const progressRefs = useRef([]);
 
   useEffect(() => {
-    const intervals = values.map((person, index) => {
-      let currentValue = 0;
-      return setInterval(() => {
-        currentValue += person.total / 100; // Increment value
+    const animateProgressBar = (bar, targetWidth) => {
+      let currentWidth = 0;
+      bar.style.width = '0%'; // Start width for animation
 
-        let newColor = "bg-white"; // Default color (white)
+      const animationInterval = setInterval(() => {
+        currentWidth += 1;
 
-        // Get color based on percentage
-        if (currentValue >= person.total * 0.8) {
-          newColor = person.colors[100];
-        } else if (currentValue >= person.total * 0.6) {
-          newColor = person.colors[80];
-        } else if (currentValue >= person.total * 0.3) {
-          newColor = person.colors[60];
+        if (currentWidth < 33) {
+          bar.style.backgroundColor = '#a8bf00'; // Light Green
+        } else if (currentWidth < 66) {
+          bar.style.backgroundColor = '#fff027'; // Yellow
+        } else if (currentWidth < 80) {
+          bar.style.backgroundColor = '#ff7927'; // Orange
         } else {
-          newColor = person.colors[30];
+          bar.style.backgroundColor = '#ff2727a6'; // Red
         }
 
-        // Ensure it doesn't exceed total value
-        if (currentValue >= person.total) {
-          currentValue = person.total;
-          clearInterval(intervals[index]); // Stop animation when target is reached
-        }
+        bar.style.width = `${currentWidth}%`;
 
-        // Update the values array with the new values and color
-        setValues((prevValues) =>
-          prevValues.map((p, i) =>
-            i === index ? { ...p, current: currentValue, color: newColor } : p
-          )
-        );
-      }, 30); // Speed of the animation (you can adjust this)
+        if (currentWidth >= targetWidth) {
+          clearInterval(animationInterval);
+        }
+      }, 30);
+    };
+
+    progressRefs.current.forEach((ref) => {
+      const bar = ref.querySelector('.percent');
+      const targetWidth = parseFloat(bar.dataset.val);
+      animateProgressBar(bar, targetWidth);
     });
 
-    // Cleanup function to clear intervals when component unmounts
-    return () => {
-      intervals.forEach((interval) => clearInterval(interval));
-    };
+    // Restart animation every 5 seconds
+    const intervals = progressRefs.current.map((ref) => 
+      setInterval(() => {
+        const bar = ref.querySelector('.percent');
+        const targetWidth = parseFloat(bar.dataset.val);
+        animateProgressBar(bar, targetWidth);
+      }, 5000)
+    );
+
+    return () => intervals.forEach((interval) => clearInterval(interval));
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col lg:flex-row items-center justify-center overflow-hidden text-white">
+    <div className="w-full h-full flex flex-col lg:flex-row items-center gap-20 md:gap-0 justify-center overflow-hidden text-white">
       {/* Left Section */}
       <div className="w-full lg:w-2/3 flex items-start justify-center flex-col bg-[#AEBD48] p-6 lg:px-20 xl:py-12 h-full">
-        <h1 className="text-5xl lg:text-8xl mt-4 lg:mt-8 mb-4 lg:mb-8">
+        <h1 className="text-3xl md:text-5xl lg:text-8xl mt-4 lg:mt-8 mb-4 lg:mb-8">
           Verordnungen
         </h1>
-        <p className="text-lg lg:text-3xl mb-1 w-3/3">
+        <p className="text-base md:text-lg lg:text-3xl mb-1 w-full md:w-2/3 lg:w-full">
           Jahresverbrauch stets im Blick behalten. Rezeptvorlagen automatisch
           erstellen lassen. Rezepte hochladen und Bewohnern zuordnen. Ganz
           einfach.
         </p>
-        <div className="mt-6 lg:mt-10">
-          <ul className="space-y-2 lg:space-y-4 text-sm lg:text-lg">
+        <div className="mt-4 md:mt-2 lg:mt-4">
+          <ul className="space-y-2 lg:space-y-4 text-sm md:text-base lg:text-lg">
             <li className="flex items-center">
-              <FaCheck className="text-lg lg:text-2xl mr-2" /> MiGeL-Barometer
+              <FaCheck className="text-base md:text-lg lg:text-2xl mr-2" /> MiGeL-Barometer
             </li>
             <li className="flex items-center">
-              <FaCheck className="text-lg lg:text-2xl mr-2" /> Rezeptmanagement
+              <FaCheck className="text-base md:text-lg lg:text-2xl mr-2" /> Rezeptmanagement
             </li>
             <li className="flex items-center">
-              <FaCheck className="text-lg lg:text-2xl mr-2" /> Automatische
-              Vorlagen
+              <FaCheck className="text-base md:text-lg lg:text-2xl mr-2" /> Automatische Vorlagen
             </li>
             <li className="flex items-center">
-              <FaCheck className="text-lg lg:text-2xl mr-2" />{" "}
-              Benachrichtigungen (Apotheke)
+              <FaCheck className="text-base md:text-lg lg:text-2xl mr-2" /> Benachrichtigungen (Apotheke)
             </li>
           </ul>
         </div>
       </div>
 
       {/* Right Section */}
-      <div className="w-full lg:w-1/3 lg:bg-white bg-[#AEBD48] lg:relative flex justify-center items-center lg:justify-end">
-        <div className="p-6 w-full rounded-lg absolute lg:-left-20 -skew-x-6">
-          <div className="bg-gradient-to-r from-[#abbc33] to-[#abbc33]/50 text-white border border-white py-2 px-4 rounded-t-lg font-bold text-lg">
+      <div className="w-full lg:w-1/3 bg-[#AEBD48] lg:bg-white lg:relative flex justify-center items-center lg:justify-end">
+        <div className="p-4 md:p-6 lg:p-6 w-full rounded-lg lg:absolute lg:-left-20 -skew-x-6">
+          <div className="bg-gradient-to-r from-[#abbc33] to-[#abbc33]/50 text-white border border-white py-2 px-4 rounded-t-lg font-bold text-sm md:text-base lg:text-lg">
             Abteilung B
           </div>
           <div className="bg-white shadow-lg p-4 rounded-b-lg">
-            {values.map((person, index) => (
-              <div
-                key={index}
-                className="flex justify-between text-black items-center py-2"
-              >
-                <span>{person.name}</span>
-                <div className="w-48 h-6 bg-gray-300 rounded-lg relative overflow-hidden">
-                  {/* Background bar */}
-                  <div
-                    className={`absolute top-0 left-0 h-full ${person.color}`}
-                    style={{
-                      width: `${(person.current / person.total) * 100}%`,
-                    }}
-                  />
-                  {/* Value text */}
-                  <div className="absolute inset-0 flex items-center justify-center text-black font-medium">
-                    CHF {person.current.toFixed(2)} / CHF{" "}
-                    {person.total.toFixed(2)}
-                  </div>
-                </div>
-              </div>
-            ))}
+      {[
+        { name: "Meier, Hans", current: 322.33, total: 408.01, percentage: 95, color: 'bg-[#ff7927]' },
+        { name: "Müller, Erika", current: 650.46, total: 867.28, percentage: 75, color: 'bg-[#ff7927]' },
+        { name: "Schmidt, Elisabeth", current: 204.00, total: 408.01, percentage: 50, color: 'bg-[#fff027]' },
+        { name: "Fischer, Werner", current: 199.50, total: 798.00, percentage: 25, color: 'bg-[#a8bf00]' }
+      ].map((person, index) => (
+        <div
+          key={index}
+          className="flex justify-between text-black items-center py-2"
+          ref={el => progressRefs.current[index] = el}
+        >
+          {/* Person Name */}
+          <span className="text-xs md:text-sm lg:text-base">
+            {person.name}
+          </span>
+
+          {/* Progress Bar */}
+          <div className="w-28 md:w-36 lg:w-48 h-3 md:h-4 lg:h-6 bg-gray-300 rounded-lg relative overflow-hidden">
+            <div
+              className={`absolute top-0 left-0 h-full percent`}
+              data-val={person.percentage}
+              style={{
+                width: '0%', // Start width for animation
+              }}
+            >
+              <small className="absolute top-1/2 transform -translate-y-1/2 left-1 text-black text-[0.5rem] md:text-[0.6rem] lg:text-xs font-semibold whitespace-nowrap">
+                CHF {person.current.toFixed(2)} / CHF {person.total.toFixed(2)}
+              </small>
+            </div>
           </div>
+        </div>
+      ))}
+    </div>
           <div className="relative flex justify-end mt-4">
-            <button className="bg-white hover:bg-black hover:text-white text-gray-700 py-2 px-4 rounded-lg shadow-lg hover:shadow-md">
+            <button className="bg-[#f1f1f1] hover:bg-black hover:text-white text-gray-700 py-1 md:py-2 px-3 md:px-4 rounded-lg shadow-lg hover:shadow-md text-xs md:text-sm lg:text-base">
               Rezeptvorlage erstellen
             </button>
-            {/* CSS Arrow */}
             <img
-              className="absolute -bottom-4 -right-8 w-20"
+              className="absolute -bottom-4 -right-6 w-10 md:w-14 lg:w-20"
               src={arrow}
               alt="arrow"
             />
